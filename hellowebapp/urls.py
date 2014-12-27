89aa31cb6444ea583290bab25ef9cb599edeeed1
Tracy Osborn <tracy@weddinglovely.com>
2014-12-26 18:13:42 -0800

    End of Chapter 11

--- hellowebapp/urls.py
+++ hellowebapp/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.contrib.auth.views import password_reset, password_reset_done, password_reset_confirm, password_reset_complete
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, RedirectView
 
 from collection.backends import MyRegistrationView
 
@@ -10,10 +10,17 @@ urlpatterns = patterns('',
     url(r'^$', 'collection.views.index', name='home'),
     url(r'^about/$', TemplateView.as_view(template_name='about.html'), name='about'),
     url(r'^contact/$', TemplateView.as_view(template_name='contact.html'), name='contact'),
+
+    url(r'^things/$', RedirectView.as_view(pattern_name='browse')),
     url(r'^things/(?P<slug>[-\w]+)/$', 'collection.views.thing_detail', name='thing_detail'),
     # new line we're adding!
     url(r'^things/(?P<slug>[-\w]+)/edit/$', 'collection.views.edit_thing', name='edit_thing'),
 
+    url(r'^browse/$', RedirectView.as_view(pattern_name='browse')),
+    url(r'^browse/name/$', 'collection.views.browse_by_name', name='browse'),
+    url(r'^browse/name/(?P<initial>[-\w]+)/$', 'collection.views.browse_by_name', name='browse_by_name'),
+
+    # password reset urls
     url(r'^accounts/password/reset/$', password_reset, 
         {'template_name': 'registration/password_reset_form.html'}, 
         name="password_reset"),
