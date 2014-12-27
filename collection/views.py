952240f29bdeab2bc6d59eaffc0556c360a13890
Tracy Osborn <tracy@weddinglovely.com>
2014-12-26 17:12:17 -0800

    End of Chapter 5

--- collection/views.py
+++ collection/views.py
@@ -3,8 +3,10 @@ from django.shortcuts import render
 
 def index(request):
     number = 6
+
     # don't forget the quotes because it's a string, not an integer
     thing = "Thing name"
+
     return render(request, 'index.html', {
         'number': number,
         # don't forget to pass it in, and the last comma
